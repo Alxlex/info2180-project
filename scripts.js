a79a3b7516d79c51b63a662763693f8ed5415d5b
Alxlex <salmon.alex235@gmail.com>
2022-11-02 21:21:27 -0500

    Updated Javascript File

--- scripts.js
+++ scripts.js
@@ -3,18 +3,19 @@
 window.onload = function(){
 
     var submitButton = document.getElementsByClassName("btn")[1];
-    submitButton.addEventListener("click", () => {
-        preventDefault(); 
 
-        var email = document.getElementById("email").value;
+    submitButton.addEventListener("click", click);
+}
 
-        console.log(email);
 
-        if(email != ""){
-            document.getElementsByClassName("message")[0].innerHTML = "Thank You! Your email address " +email+ " has been added to our mailing list";
-        }else{
-            document.getElementsByClassName("message")[0].innerHTML = "";
-        }
-    });
+function click(ev){
 
+    ev.preventDefault();
+    var email = document.getElementById("email").value;
+
+    if(email != ""){
+        document.getElementsByClassName("message")[0].innerHTML = "Thank You! Your email address "+email+" has been added to our mailing list";
+    }else{
+        document.getElementsByClassName("message")[0].innerHTML = "";
+    }
 }
